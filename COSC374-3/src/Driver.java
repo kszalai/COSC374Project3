d1ec17ad68fe61b8b4973ae0a6fc64d466b79ff7
rkendall0717 <rkendall0717@gmail.com>
2015-04-11 13:40:51 -0400

    Robs driver changes

--- COSC374-3/src/Driver.java
+++ COSC374-3/src/Driver.java
@@ -3,5 +3,6 @@ public class Driver {
 
 	public static void main(String[]args){
 		System.out.println("This is Kyle testing the driver commits");
+		System.out.println("Rob is trying the driver commits"); // yay
 	}
 }
