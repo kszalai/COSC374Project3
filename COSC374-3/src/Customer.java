086a7fe9ec9fe29515f6fe1bc04d2dfb4ac178b3
rkendall0717 <rkendall0717@gmail.com>
2015-04-11 15:41:45 -0400

    Customer Changes

--- COSC374-3/src/Customer.java
+++ COSC374-3/src/Customer.java
@@ -1,11 +1,16 @@
 public class Customer {
 
+	MoneyOrder[] data = new MoneyOrder[10];
+	int location = 0;
 
 	// constructor
 	public void insert(String first, String last, String strNumber,
 			String strName, String strType, String city, String state,
 			String zip, String MonOrdID, double amount, int MonOrdNums)
-		{	
 
-		}
+	{
+		data[location] = new MoneyOrder(first, last, strNumber, strName,
+				strType, city, state, zip, MonOrdID, amount, MonOrdNums);
+		location++;
+	}
 }
