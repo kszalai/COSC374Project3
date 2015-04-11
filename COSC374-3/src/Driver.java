5509098426cd3511832a71c90e4810936fee0618
rkendall0717 <rkendall0717@gmail.com>
2015-04-11 13:51:54 -0400

    Rob's Driver changes

--- COSC374-3/src/Driver.java
+++ COSC374-3/src/Driver.java
@@ -1,10 +1,27 @@
+import java.util.*;
 
 public class Driver {
 
-	public static void main(String[]args){
-		System.out.println("This is Kyle testing the driver commits");
-		System.out.println("Rob is trying the driver commits"); 
-		System.out.println("This is another change from Kyle");
-		System.out.println("Welcome to MemeCoin");
+	public static Scanner keyboard = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome");
+
+		do {
+
+			System.out.println("1. Send Money Order");
+			System.out.println("2. Quit");
+			int response = keyboard.nextInt();
+
+			switch (response) {
+			case 1:
+				SendMoneyOrder();
+				break;
+			case 2:
+				System.out.println("GoodBye");
+			}
+
+		} while (response > 0 && response < 3);
 	}
 }
