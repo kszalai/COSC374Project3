b3934c53e1d71f7f987cd592c327a859d18ca51a
rkendall0717 <rkendall0717@gmail.com>
2015-04-11 14:40:08 -0400

    Rob's changes to MoneyOrder

--- COSC374-3/src/MoneyOrder.java
+++ COSC374-3/src/MoneyOrder.java
@@ -1,4 +1,18 @@
 
 public class MoneyOrder {
-
+// variables for customer info
+	private String fName;
+	private String lName;
+	private String strNum;
+	private String strName;
+	private String strType;
+	private String city;
+	private String state;
+	private String zip;
+	private String ID;
+	
+	// variables for money order info
+	private double amount;
+	private int MonOrdnums;
+	private int IDnums;
 }
