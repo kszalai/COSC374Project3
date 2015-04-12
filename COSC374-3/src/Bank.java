1a91785db6eb7cc865ec919aa4c74d23ccb158ce
Greg <gmckinst@emich.edu>
2015-04-12 16:46:36 -0400

    Added getBankSignatures to bank class.
    Signed-off-by: Greg <gmckinst@emich.edu>

--- COSC374-3/src/Bank.java
+++ COSC374-3/src/Bank.java
@@ -1,4 +1,15 @@
 
 public class Bank {
+	private String bankSignatureList[];
+	
+	/*
+	 * Gets a list of bank signatures from a
+	 * file and stores them in a signature list.
+	 * Then returns the list.
+	 */
+	public String[] getBankSignatures()
+	{
+		return bankSignatureList;
+	}
 
 }
--- COSC374-3/src/Merchant.java
+++ COSC374-3/src/Merchant.java
@@ -1,4 +1,28 @@
 
 public class Merchant {
+	
+	/*
+	 * A method that accepts a list if uniqueness strings from the bank
+	 * and a moneyorder object. If a signature matches the moneyorder, true is 
+	 * returned, else false is returned.
+	 * This is the merchant's step in the verification of the legitimacy
+	 * of the money order.
+	 */
+	public boolean verifyBankSigniture(String signatureList[], MoneyOrder MO)
+	{
+		//for loop to search through a list of uniqueness strings from the bank.
+		for(int i = 0; i < signatureList.length(); i++)
+		{
+			//an if statement tests each string and compares it to the moneyorder's
+			// signature to find a match.
+			if(signatureList[i].equalsIgnoreCase(MO.getMOID()))
+			{
+				return true;
+			}
+		}
+		//No match was found, person is lying.
+		return false;
+		
+	}
 
 }
