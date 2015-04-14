e1b33416f56cefc367e02c4aa216b763e409e16f
Greg <gmckinst@emich.edu>
2015-04-13 22:26:59 -0400

    Added method to obtain the bank's digital signature. Added method to save the signature onto a text file.
    Signed-off-by: Greg <gmckinst@emich.edu>

--- COSC374-3/src/Bank.java
+++ COSC374-3/src/Bank.java
@@ -1,13 +1,37 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 public class Bank {
 	private String bankSignatureList[];
 	private String fileName = "BankSignatures.txt";
-	private int[] publickey = {29,328583};
-	private int[] privatekey = {169349,328583};
+	private int[] publicKey = {29,328583};
+	private int[] privateKey = {169349,328583};
+	
+	/*
+	 * Returns the public key to a 
+	 * calling function.
+	 */
+	public int[] getPublicKey()
+	{
+		//returns the public key {e,n}
+		return publicKey;
+	}
+	
+	/*
+	 * Returns the private key to a 
+	 * calling function.
+	 */
+	public int[] getPrivateKey()
+	{
+		//returns the private key {e,n}
+		return privateKey;
+	}
 	
 	/*
 	 * Gets a list of bank signatures from a
@@ -47,23 +71,59 @@ public class Bank {
 		
 		//Set up a buffered reader to read each line from the file.
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		//String builder with a capacity 500 characters long
+		//is this long enough too long? eh?
+		StringBuilder sb = new StringBuilder(500);
 
 		try {
-				//get the first string at the top of the file
-				//This should be the amount, but it doesn't matter
-				//strings in the file.
-				String line = br.readLine();
+				/*
+				 * This will read each character individually
+				 * and then the RSA function will be applied to the 
+				 * character (character is read as an int).
+				 * The result will be put into a string builder
+				 * which will then be inserted into the file
+				 * at the bottom of the file.
+				 */
+				int character = br.read();
 					
-				while(line != null)
+				while(character != -1)
 				{
-
+					int c = (int) Math.pow(character,e)%n;
+					sb.append(c);
 				}
 
 			} finally {
 
 				br.close();
 			}	
-				
+		
+		storeDigitalSignature(fileName, sb.toString());
+	}
+	
+	/*
+	 * This method excepts a file name and a digital signature string
+	 * and stores the digital signature on the file by appending it
+	 * to the end of the file.
+	 */
+	
+	public static void storeDigitalSignature(String fileName, String digitalSignatire) throws IOException{
+		
+		File f = new File(fileName);
+		
+		if(!f.exists())
+		{
+			f.createNewFile();
+		}
+		
+		try {
+		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
+		    out.println(digitalSignatire);
+		    out.close();
+		} catch (IOException e) {
+		    System.out.println("File does not exist.");
+		}
+		
+		System.out.println("Digital Signature Written to: " + fileName);
 		
 	}
 	
