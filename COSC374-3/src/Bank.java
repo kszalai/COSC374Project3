bc737c3f01d62832789d444982e57672040754aa
Kyle <kyleszalai@gmail.com>
2015-04-19 17:40:04 -0400

    Updated the bank sginature method to not read from file
    Signed-off-by: Kyle <kyleszalai@gmail.com>

--- COSC374-3/src/Bank.java
+++ COSC374-3/src/Bank.java
@@ -7,12 +7,12 @@ public class Bank {
 	private static int[] privateKey = {169349,328583};
 	private static MoneyOrder[] moneyOrder = new MoneyOrder[100];
 	private static int moneyOrderCount = 0;
-	
+
 	public Bank(MoneyOrder[] moneyOrder)
 	{
 		Bank.moneyOrder = moneyOrder;
 	}
-	
+
 	/*
 	 * Returns the public key to a 
 	 * calling function.
@@ -22,7 +22,7 @@ public class Bank {
 		//returns the public key {e,n}
 		return publicKey;
 	}
-	
+
 	/*
 	 * Returns the private key to a 
 	 * calling function.
@@ -32,7 +32,7 @@ public class Bank {
 		//returns the private key {e,n}
 		return privateKey;
 	}
-	
+
 	/*
 	 * Gets a list of bank signatures from a
 	 * file and stores them in a signature list.
@@ -41,16 +41,16 @@ public class Bank {
 	public static String[] getBankSignatures()
 	{
 		try{
-		retrieveSignaturesFromFile();
+			retrieveSignaturesFromFile();
 		}catch(IOException e)
 		{
 			System.out.println("File does not exist yet!");
 			return null;
 		}
-		
+
 		return bankSignatureList;
 	}
-	
+
 	/*
 	 * This function implements the RSA Digital Signature
 	 * algorithm for the bank, it saves the digital signature
@@ -59,8 +59,8 @@ public class Bank {
 	 * each line is run through the RSA algorithm and the private
 	 * key and public keys are both saved for later use.
 	 */
-	
-	public void makeBankDigitalSignature(String fileName) throws IOException
+
+	public void makeBankDigitalSignature() throws IOException
 	{
 		//RSA variables.
 		int p = 457;	//A prime
@@ -69,73 +69,66 @@ public class Bank {
 		int tn = 327408;// (p-1)x(q-1)
 		int e = 29;		//selected integer
 		int d = 169349; //Gotten from wolfram alpha.
-		
-		//Set up a buffered reader to read each line from the file.
-		BufferedReader br = new BufferedReader(new FileReader(fileName));
+
 		//String builder with a capacity 500 characters long
 		//is this long enough too long? eh?
 		StringBuilder sb = new StringBuilder(500);
 
-		try {
-				/*
-				 * This will read each character individually
-				 * and then the RSA function will be applied to the 
-				 * character (character is read as an int).
-				 * The bank raises the message multiplied by
-				 * the customer's random number by d.
-				 * The result will be put into a string builder
-				 * which will then be inserted into the file
-				 * at the bottom of the file.
-				 */
-				int character = br.read();
-					
-				while(character != -1)
-				{
-					int c = (int) Math.pow(character,d)%n;
-					sb.append(c);
-				}
+		/*
+		 * This will read each character individually
+		 * and then the RSA function will be applied to the 
+		 * character (character is read as an int).
+		 * The bank raises the message multiplied by
+		 * the customer's random number by d.
+		 * The result will be put into a string builder
+		 * which will then be inserted into the file
+		 * at the bottom of the file.
+		 */
+		String signature = "B@nK$1GnAtUR3";
 
-			} finally {
+		for(int i=0;i<signature.length();i++)
+		{
+			int character = signature.charAt(i);
+			int c = (int) Math.pow(character,d)%n;
+			sb.append(c);
+		}	
 
-				br.close();
-			}	
-		
-		storeDigitalSignature(fileName, sb.toString());
+		storeDigitalSignature("bankSigs.txt", sb.toString());
 		for(int i=0;i<moneyOrder.length;i++)
 		{
 			moneyOrder[i].setBankSig(Integer.parseInt(sb.toString()));
 		}
 	}
-	
+
 	/*
 	 * This method excepts a file name and a digital signature string
 	 * and stores the digital signature on the file by appending it
 	 * to the end of the file.
 	 */
-	
+
 	public static void storeDigitalSignature(String fileName, String digitalSignature) throws IOException{
-		
+
 		File f = new File(fileName);
-		
+
 		if(!f.exists())
 		{
 			f.createNewFile();
 		}
-		
+
 		try {
-		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
-		    out.println(digitalSignature);
-		    out.close();
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
+			out.println(digitalSignature);
+			out.close();
 		} catch (IOException e) {
-		    System.out.println("File does not exist.");
+			System.out.println("File does not exist.");
 		}
-		
+
 		System.out.println("Digital Signature Written to: " + fileName);
-		
+
 	}
-	
-	
-	
+
+
+
 	/*
 	 * A method that obtains all bank uniqueness 
 	 * strings from a file and stores them in the
@@ -153,37 +146,37 @@ public class Bank {
 			//This should be the total number of uniqueness 
 			//strings in the file.
 			String line = br.readLine();
-			
+
 			//parse the first line into an integer.
 			totalEntries = Integer.parseInt(line);
-			
+
 			bankSignatureList = new String[totalEntries];
-			
-		for(int i = 0; i < totalEntries; i++)
-		{
 
-			bankSignatureList[i] = br.readLine();
+			for(int i = 0; i < totalEntries; i++)
+			{
 
-		}
+				bankSignatureList[i] = br.readLine();
+
+			}
 
 		} finally {
 
-		br.close();
+			br.close();
 		}			
 	}
-	
+
 	public static void addBankOrder(MoneyOrder x)
 	{
 		moneyOrder[moneyOrderCount] = x;
 		moneyOrderCount++;
 	}
-	
+
 	public static MoneyOrder compareBankUniqueIDs(MoneyOrder x)
 	{
 		for (int i = 0; i < moneyOrderCount; i++)
 		{
 			if (x.getMOID() == moneyOrder[i].getMOID())
-			return moneyOrder[i];	
+				return moneyOrder[i];	
 		}
 		return null;
 	}
--- COSC374-3/src/Driver.java
+++ COSC374-3/src/Driver.java
@@ -131,7 +131,7 @@ public class Driver {
 		//Bank signs blinded moneyOrder
 		try 
 		{
-			theBank.makeBankDigitalSignature("bankSigs.txt");
+			theBank.makeBankDigitalSignature();
 		} 
 		catch (IOException e) 
 		{
